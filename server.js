069c90174bafc0e1407c810702ed066b2632911c
Rohan Shankar <roshank@live.unc.edu>
2022-04-19 22:05:54 -0400

    no way right

--- server.js
+++ server.js
@@ -45,16 +45,6 @@ if (args.log == 'true') {
     app.use(morgan('combined'), { stream: WRITESTREAM });
 } 
 
-if (args.debug || args.d){
-    app.get('/app/log/access/', (req, res, next) => {
-    const stmt = logdb.prepare('SELECT * FROM accessLog').all();
-    res.status(200).json(stmt);
-    })
-    app.get('/app/error/', (req, res, next) => {
-      throw new Error('Error');
-    })
-}
-
 app.use((req, res, next) => {
     let logdata = {
         remoteaddr: req.ip,
@@ -69,11 +59,19 @@ app.use((req, res, next) => {
         useragent: req.headers['user-agent']
     }
     const stmt = logdb.prepare('INSERT INTO accessLog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent);
+    stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent);
     next();
 });
 
-
+if (args.debug || args.d){
+    app.get('/app/log/access/', (req, res, next) => {
+    const stmt = logdb.prepare('SELECT * FROM accessLog').all();
+    res.status(200).json(stmt);
+    })
+    app.get('/app/error/', (req, res, next) => {
+      throw new Error('Error');
+    })
+}
 
 
 //------------------------------------------------------------------------
